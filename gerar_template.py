4747afae53e03ef2a69eb6f9537cea956d5be71c
Tarciof2025 <tarciofjesus@gmail.com>
2025-05-17 00:47:27 -0300

    Update gerar_template.py

--- gerar_template.py
+++ gerar_template.py
@@ -1,7 +1,44 @@
-git config --global user.name "Tarcio"
-git config --global user.email "tarciofjesus@gmail.com"
-git add .
-git commit -m "Primeiro commit"
-git branch -M main  # ou master, conforme seu caso
-git remote add origin https://github.com/Tarciof2025/sistema_remocoes.git
-git push -u origin main  # ou master
+import pandas as pd
+
+def gerar_template(nome_arquivo="template_remocoes.xlsx"):
+    # Define as colunas principais que você mencionou (pode ajustar conforme precisar)
+    colunas = [
+        "Placa",
+        "Deposito/Vendido",
+        "Modelo",
+        "Chassi",
+        "Cliente",
+        "Pátio",
+        "Segmento",
+        "Contato",
+        "Telefone",
+        "UF",
+        "Cidade",
+        "Endereço",
+        "Data solicitação/Agendamento",
+        "SLA",
+        "Data Real remoção",
+        "Data da frustada",
+        "Impedimento",
+        "Coluna1",
+        "Status",
+        "Motivo",
+        "Responsável",
+        "Data Nova Tratativa",
+        "Endereço Nova Tratativa Scope",
+        "Data da ultima leitura",
+        "Endereço do Rastreador",
+        "Fonte da Confirmação de Endereço",
+        "Observação",
+        "Data Impedimento/Frustada",
+        "Ano",
+        "Mês",
+        "SEMANA",
+        "Confirmação de Retirada",
+        "Tempo para remoção",
+        "target",
+        "Dias",
+        "Status dias"
+    ]
+
+    # Cria um DataFra
