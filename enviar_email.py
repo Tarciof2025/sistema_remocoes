c284107d944aa3c71df14b98fdc822ddf0feb9ea
Tarciof2025 <tarciofjesus@gmail.com>
2025-05-17 00:46:59 -0300

    Update enviar_email.py

---
+++ enviar_email.py
@@ -0,0 +1,7 @@
+git config --global user.name "Tarcio"
+git config --global user.email "tarciofjesus@gmail.com"
+git add .
+git commit -m "Primeiro commit"
+git branch -M main  # ou master, conforme seu caso
+git remote add origin https://github.com/Tarciof2025/sistema_remocoes.git
+git push -u origin main  # ou master
